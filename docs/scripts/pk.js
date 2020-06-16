347d33120c5325be6aaf9f1550eeab6f236b5b33
The Rats <dev@rats.world>
2020-06-17 00:54:50 +0930

    Update pk.js

--- docs/scripts/pk.js
+++ docs/scripts/pk.js
@@ -3,50 +3,46 @@ async function updatePK(){
     const shortFormatter = new Intl.ListFormat('en', { style: 'narrow', type: 'unit' })
     const longFormatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' })
         
-        
-    if(sessionStorage.getItem("count")){
-        $("#ratCount").text(sessionStorage.getItem("count"))
-        $("#daysSince").text(sessionStorage.getItem("days"))
-        $("#ratIDs").text(sessionStorage.getItem("ids"))
-        $("#ratNames").text(sessionStorage.getItem("names"))
-        $("#fronter0Name").text(sessionStorage.getItem("fronter0"))
+    // if(sessionStorage.getItem("count")){
+    //     $("#ratCount").text(sessionStorage.getItem("count"))
+    //     $("#daysSince").text(sessionStorage.getItem("days"))
+    //     $("#ratIDs").text(sessionStorage.getItem("ids"))
+    //     $("#ratNames").text(sessionStorage.getItem("names"))
 
-    } else {
-        jQuery.get("https://api.pluralkit.me/v1/s/jjorc/members", function (data){
-        $("#ratCount").text(data.length)
-        sessionStorage.setItem("count",data.length)
-        let newest = data.sort((a,b)=>{
-            a = new Date(a.created)
-            b = new Date(b.created)
-            return b - a
-        })
-        let hours = (Date.now() - new Date(newest[0].created)) / 1000 / 60 / 60
-        sessionStorage.setItem("days",parseInt(hours))
-        $("#daysSince").text(parseInt(hours))
+    // } else {
+    //     jQuery.get("https://api.pluralkit.me/v1/s/jjorc/members", function (data){
+    //     $("#ratCount").text(data.length)
+    //     sessionStorage.setItem("count",data.length)
+    //     let newest = data.sort((a,b)=>{
+    //         a = new Date(a.created)
+    //         b = new Date(b.created)
+    //         return b - a
+    //     })
+    //     let hours = (Date.now() - new Date(newest[0].created)) / 1000 / 60 / 60
+    //     sessionStorage.setItem("days",parseInt(hours))
+    //     $("#daysSince").text(parseInt(hours))
         
         
-        // Get IDs, yeet them into a list
-        var idList = []
-        for (i in data) {
-          memberID = data[i].id
-          idList.push(memberID)
-        }
-        sessionStorage.setItem("ids",shortFormatter.format(idList))
-        $("#ratIDs").text(sessionStorage.getItem("ids"))
+    //     // Get IDs, yeet them into a list
+    //     var idList = []
+    //     for (i in data) {
+    //       memberID = data[i].id
+    //       idList.push(memberID)
+    //     }
+    //     sessionStorage.setItem("ids",shortFormatter.format(idList))
+    //     $("#ratIDs").text(sessionStorage.getItem("ids"))
         
-        // Get display names if available, else get names
-        var nameList = []
-        for (i in data) {
-          memberName = data[i].display_name || data[i].name
-          nameList.push(memberName)
-        }
-        sessionStorage.setItem("names",shortFormatter.format(nameList))
-        $("#ratNames").text(sessionStorage.getItem("names"))
+    //     // Get display names if available, else get names
+    //     var nameList = []
+    //     for (i in data) {
+    //       memberName = data[i].display_name || data[i].name
+    //       nameList.push(memberName)
+    //     }
+    //     sessionStorage.setItem("names",shortFormatter.format(nameList))
+    //     $("#ratNames").text(sessionStorage.getItem("names"))
         
-    })
-    }
-
-
+    // })
+    // }
 
 
     //   // get desc + sysname
@@ -62,12 +58,6 @@ async function updatePK(){
     // 	})
     // }
     
-
-
-
-
-
-
 
 
     // get fronter name list
@@ -108,7 +98,7 @@ async function updatePK(){
 
 
         
-        $("#fronterID").attr("src", fronterAvatarList[0]).src;
+        $("#fronterID").attr("src", fronterAvatarList[0]);
           }
 
         })    
