826c63a4619af2ea2f3530fe4cb2fda6f7262830
The Rats <dev@rats.world>
2020-07-05 08:49:23 +0930

    commented out stuff that isn't used

--- docs/scripts/pk.js
+++ docs/scripts/pk.js
@@ -23,26 +23,26 @@ async function updatePK() {
   // if count is in session storage, assume days/ids/names are too, and get them
   if (sessionStorage.getItem("count")) {
       $("#ratCount").text(sessionStorage.getItem("count"));
-      $("#daysSince").text(sessionStorage.getItem("days"));
-      $("#ratIDs").text(sessionStorage.getItem("ids"));
-      $("#ratNames").text(sessionStorage.getItem("names"));
+  //  $("#daysSince").text(sessionStorage.getItem("days"));
+  //  $("#ratIDs").text(sessionStorage.getItem("ids"));
+  //  $("#ratNames").text(sessionStorage.getItem("names"));
 
   } else {
       // if count isn't in session storage, harasses the pluralkit API and gets #ratCount, #daysSince, #ratIDs, #ratNames
       jQuery.get("https://api.pluralkit.me/v1/s/jjorc/members", function(data) {
           // #ratCount stuff
           $("#ratCount").text(data.length);
-          sessionStorage.setItem("count", data.length);
+          sessionStorage.setItem("count", text(data.length));
           // #daysSince stuff
-          let newest = data.sort((a, b) => {
-              a = new Date(a.created);
-              b = new Date(b.created);
-              return b - a;
-          });
+          // let newest = data.sort((a, b) => {
+          //     a = new Date(a.created);
+          //     b = new Date(b.created);
+          //     return b - a;
+          // });
 
-          let hours = (Date.now() - new Date(newest[0].created)) / 1000 / 60 / 60;
-          sessionStorage.setItem("days", parseInt(hours));
-          $("#daysSince").text(parseInt(hours));
+          // let hours = (Date.now() - new Date(newest[0].created)) / 1000 / 60 / 60;
+          // sessionStorage.setItem("days", parseInt(hours));
+          // $("#daysSince").text(parseInt(hours));
 
           // // #ratIDs stuff
           // var idList = [];
@@ -67,19 +67,19 @@ async function updatePK() {
 
 // /s/jjorc stuff
   // if desc is in session storage, assume sysName is too, and get them both
-  if (sessionStorage.getItem("desc")) {
-      $("#ratDesc").text(sessionStorage.getItem("desc"));
-      $("#ratSysName").text(sessionStorage.getItem("sysName"));
+  // if (sessionStorage.getItem("desc")) {
+  //     $("#ratDesc").text(sessionStorage.getItem("desc"));
+  //     $("#ratSysName").text(sessionStorage.getItem("sysName"));
 
-  } else {
-      // if desc isn't in session storage, harasses the pluralkit API and gets #ratDesc and #ratSysName
-      jQuery.get("https://api.pluralkit.me/v1/s/jjorc", function(data) {
-          sessionStorage.setItem("desc", data.description);
-          sessionStorage.setItem("sysName", data.name);
-          $("#ratDesc").text(sessionStorage.getItem("desc"));
-          $("#ratSysName").text(sessionStorage.getItem("sysName"));
-      });
-  }
+  // } else {
+  //     // if desc isn't in session storage, harasses the pluralkit API and gets #ratDesc and #ratSysName
+  //     jQuery.get("https://api.pluralkit.me/v1/s/jjorc", function(data) {
+  //         sessionStorage.setItem("desc", data.description);
+  //         sessionStorage.setItem("sysName", data.name);
+  //         $("#ratDesc").text(sessionStorage.getItem("desc"));
+  //         $("#ratSysName").text(sessionStorage.getItem("sysName"));
+  //     });
+  // }
 
 // /s/jjorc/fronters stuff
   // if fronters is in session storage, assume fronter0Name/fronter0Pronouns/fronter0Avatar/fronter0Desc/fronter0Avatar are too, and get them
